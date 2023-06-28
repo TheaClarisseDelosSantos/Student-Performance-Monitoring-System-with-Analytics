5d70ba823758454266161691c92f2c2ef25e827d
Thea Clarisse P. Delos Santos <theadelossantos08@gmail.com>
2023-06-28 11:23:46 +0800

    errormessage

--- App/src/app/pages/login/login.page.ts
+++ App/src/app/pages/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,13 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
   passwordVisible: boolean = false;
+  validationUserMessage: any;
+  validationFormUser!: FormGroup;
 
   togglePasswordVisibility() {
     this.passwordVisible = !this.passwordVisible;
   }
-  constructor() { }
+
+
+  constructor(public formbuider: FormBuilder) { }
 
   ngOnInit() {
+    this.validationUserMessage = {
+      email:[
+        {type:"required",message:"Please enter your email"},
+        {type:"pattern",message:"Incorrect Email. Try again."}
+      ],
+      password:[
+        {type:"required", message:"Please enter your password"},
+        {type:"minlength",message:"The password must be at least 5 characters or more"}
+      ]
+    };
+
+    this.validationFormUser = this.formbuider.group({
+      email:new FormControl('',Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('',Validators.compose([
+        Validators.required,
+        Validators.minLength(5)
+      ]))
+    })
+  }
+
+  LoginUser(value:any){
+    console.log("Am logged in")
   }
 
 }
